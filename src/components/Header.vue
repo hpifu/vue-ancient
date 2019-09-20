be77603958124394bd67935ecbeae84376fa0e08
hatlonely <hatlonely@gmail.com>
2019-09-20 21:19:44 +0800

    update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,5 +8,17 @@
       <span class="font-weight-light cyan--text">ancient</span>
     </v-toolbar-title>
     <v-spacer></v-spacer>
+    <v-text-field
+      v-model="search"
+      prepend-inner-icon="search"
+      :label="'搜索 ' + $config.org + ' 账户'"
+      single-line
+      rounded
+      clearable
+      clear-icon="clear"
+      class="mt-9"
+      filled
+      full-width
+    ></v-text-field>
   </v-app-bar>
 </template>
